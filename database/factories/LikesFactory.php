dd232617609eb320fc114b4ea9603dfdeab5f852
Abhishek Shrivastava <abhishek.shrivastava1987@gmail.com>
2018-06-22 23:40:34 +0530

    Created Factory For All

--- database/factories/LikesFactory.php
+++ database/factories/LikesFactory.php
@@ -4,6 +4,8 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Model\Likes::class, function (Faker $faker) {
     return [
-        //
+        'user_id' => function() {
+            return User::all()->random();
+        }
     ];
 });
