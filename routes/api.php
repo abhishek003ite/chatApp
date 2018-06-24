8eec4707f9b632159ce8bb47f95bc238d7d6cbfd
Abhishek Shrivastava <abhishek.shrivastava1987@gmail.com>
2018-06-24 15:12:44 +0530

    JWT installed and configured

--- routes/api.php
+++ routes/api.php
@@ -12,3 +12,17 @@ Route::apiResource('/question/{question}/reply', 'ReplyController');
 //Like or Dislike Route
 Route::post('/{reply}/like', 'LikesController@likeIt');
 Route::delete('/{reply}/like', 'LikesController@unLikeIt');
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
